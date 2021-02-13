43ba138bdad68fffa329abac013dc62a9688cb00
Tianyi Xu <nora.xu@mail.utoronto.ca>
2021-02-13 01:45:40 -0500

    lab2 fixed transmit error

--- programming labs/lab2/packet.c
+++ programming labs/lab2/packet.c
@@ -27,33 +27,46 @@ void packetToString(struct packet* file_packet, char* packet_str){
 }
 
 void stringToPacket(char* packet_str, struct packet* file_packet){
-   char* token = strtok(packet_str, ":");
-   int i = 0;
-   int ptr = 0;
-   while(i != 5){
-      if(i == 0){
-         file_packet->total_frag = atoi(token);
-         ptr = ptr + strlen(token) + 1;
+   int prev_ptr = 0;
+   int curr_ptr = 0;
+   char buff[100];
+   for(int field_idx = 0; field_idx < 5; field_idx++){
+      while(*(packet_str + curr_ptr) != ':' && field_idx != 4){
+         curr_ptr++;
       }
-      if(i == 1){
-         file_packet->frag_no = atoi(token);
-         ptr = ptr + strlen(token) + 1;
+      // get the field value
+      if(field_idx == 0){
+         memcpy(buff, packet_str+prev_ptr, curr_ptr - prev_ptr);
+         buff[curr_ptr-prev_ptr] = '\0';
+         file_packet->total_frag = atoi(buff);
+         printf("total_frag:%d\n", file_packet->total_frag);
       }
-      if(i == 2){
-         file_packet->size = atoi(token);
-         ptr = ptr + strlen(token) + 1;
+      if(field_idx == 1){
+         memcpy(buff, packet_str+prev_ptr, curr_ptr - prev_ptr);
+         buff[curr_ptr-prev_ptr] = '\0';
+         file_packet->frag_no = atoi(buff);
+         printf("frag_no: %d\n", file_packet->frag_no);
       }
-      if(i == 3){
-         file_packet->filename = (char*) malloc(strlen(token)+1);
-         strcpy(file_packet->filename, token);
-         ptr = ptr + strlen(token) + 1;
+      if(field_idx == 2){
+         memcpy(buff, packet_str+prev_ptr, curr_ptr - prev_ptr);
+         buff[curr_ptr-prev_ptr] = '\0';
+         file_packet->size = atoi(buff);
+         printf("size: %d\n", file_packet->size);
       }
-      if(i == 4){
+      if(field_idx == 3){
+         file_packet->filename = (char*) malloc(curr_ptr - prev_ptr + 1);
+         memcpy(file_packet->filename, packet_str+prev_ptr, curr_ptr - prev_ptr);
+         file_packet->filename[curr_ptr-prev_ptr] = '\0';
+         printf("file_name: %s", file_packet->filename);
+      }
+      if(field_idx == 4){
          memset(file_packet->filedata, 0, DATASIZE);
-         memcpy(file_packet->filedata, packet_str + ptr, file_packet->size);
+         memcpy(file_packet->filedata, packet_str + curr_ptr, file_packet->size);
       }
-      token = strtok(NULL, ":");
-      i++;
+      
+      // incr prev_ptr
+      curr_ptr++;
+      prev_ptr = curr_ptr;
    }
 }
 
